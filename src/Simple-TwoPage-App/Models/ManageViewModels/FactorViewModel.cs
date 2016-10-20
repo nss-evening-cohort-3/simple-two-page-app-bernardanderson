655f764bc0041458bffbe0cb07505e13d38d473e
Bernard Anderson <bernardanderson@gmail.com>
2016-10-19 20:55:08 -0500

    23 min

---
+++ src/Simple-TwoPage-App/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Simple_TwoPage_App.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
